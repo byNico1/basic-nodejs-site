d096e8c8c954df48f16b0fbed0aec1355b6ab692
nicoiscoding <nicofoque0205@gmail.com>
2024-11-07 16:20:41 -0500

    adapt express

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
-import { createServer } from "http";
-import fs from "fs/promises";
+import express from "express";
 import path from "path";
 import url from "url";
 
+const app = express();
 const PORT = 5000;
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const server = createServer(async (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  let filePath = "";
-
-  try {
-    if (req.method === "GET" && req.url === "/") {
-      filePath = path.join(__dirname, "public", "index.html");
-    } else if (req.method === "GET" && req.url === "/about") {
-      filePath = path.join(__dirname, "public", "about.html");
-    } else if (req.method === "GET" && req.url === "/contact-me") {
-      filePath = path.join(__dirname, "public", "contact-me.html");
-    } else {
-      throw new Error("HTTP method not supported");
-    }
-
-    const data = await fs.readFile(filePath);
-    res.end(data);
-  } catch (error) {
-    console.log(error);
-    const data = await fs.readFile("./public/404.html");
-    res.statusCode = 404;
-    res.end(data);
-  }
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-server.listen(PORT, () => {
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "contact-me.html"));
+});
+
+app.all("*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
+
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
